5d612b17a63ac1818c5c2d3f9dcad4a0761dda51
jenil-kachhadiya <jenilkachhadiya45@gmail.com>
2026-01-16 13:38:46 +0530

    Add files via upload

---
+++ JS-PR/program.ts
@@ -0,0 +1,53 @@
+// 1. Area of Triangle
+let base: number = 10;
+let height: number = 5;
+let triangleArea: number = (base * height) / 2;
+console.log("Area of Triangle:", triangleArea);
+
+// 2. Temperature
+let celsius: number = 25;
+let fahrenheit: number = (celsius * 9/5) + 32;
+console.log("Celsius to Fahrenheit:", fahrenheit);
+
+// (a + b + c)² Formula
+let f: number = 77;
+let c: number = (f - 32) * 5/9;
+console.log("Fahrenheit to Celsius:", c);
+
+
+let a: number = 3;
+let b: number = 2;
+let c1: number = 4;
+
+let formula1: number = a*a + b*b + c1*c1 + 2*a*b + 2*a*c1 + 2*b*c1;
+console.log("(a + b + c)² =", formula1);
+
+// 3. Area of Rectangle
+let lenth: number = 8;
+let width: number = 6;
+let rectangleArea: number = lenth * width;
+console.log("Area of Rectangle:", rectangleArea);
+
+
+let radius: number = 7;
+let circleArea: number = 3.14 * radius * radius;
+console.log("Area of Circle:", circleArea);
+
+// 5. Feet to Inches Conversion
+let feet: number = 5;
+let inches: number = feet * 12;
+console.log("Feet to Inches:", inches);
+
+
+// 6. Formulas
+let formula2: number = (a - b) * (a + b);
+console.log("a² - b² =", formula2);
+
+let formula3: number = a*a - 2*a*b + b*b;
+console.log("(a - b)² =", formula3);
+
+let formula4: number = a*a + b*b + c1*c1 - 2*a*b - 2*a*c1 + 2*b*c1;
+console.log("(a - b - c)² =", formula4);
+
+let formula5: number = a*a*a - 3*a*a*b + 3*a*b*b - b*b*b;
+console.log("(a - b)³ =", formula5);
